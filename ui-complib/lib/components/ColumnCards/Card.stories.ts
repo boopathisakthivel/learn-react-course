939260aa1f7af64101a062d65bf19645d5c72020
PranavKumarB <b.pranavkumar2002@gmail.com>
2024-03-10 12:35:59 +0530

    Design Cahnges

--- ui-complib/lib/components/ColumnCards/Card.stories.ts
+++ ui-complib/lib/components/ColumnCards/Card.stories.ts
@@ -1,21 +1,41 @@
+// Card.stories.ts
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Card } from './Card';
+import { ColumnCard } from '.';
 
 const meta = {
-  title: 'Components/Card',
-  component: Card,
+  title: 'Components/ColumnCard',
+  component: ColumnCard,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
-} satisfies Meta<typeof Card>;
+} as Meta;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    title: 'this is sample card title'
+    cards: [
+      {
+        imageUrl: 'https://wac-cdn.atlassian.com/misc-assets/webp-images/Blog_360x265%402x.webp',
+        title: 'Work Life',
+        description: 'Real-life advice, inspiration, and stories from the working world today.',
+        link: 'Read our blog',
+      },
+      {
+        imageUrl: 'https://wac-cdn.atlassian.com/misc-assets/webp-images/team_anywear_illo.webp',
+        title: 'Distributed Work',
+        description: 'Advance how your distributed teams collaborate.',
+        link: 'Our evidence-based approach',
+      },
+      {
+        imageUrl: 'https://wac-cdn.atlassian.com/misc-assets/webp-images/Team%2520Playbook_360x265%402x.webp',
+        title: 'Atlassian Team Playbook',
+        description: 'Solve common team challenges with these group exercises.',
+        link: 'Check team health',
+      },
+    ],
   },
 };
--- ui-complib/lib/components/ColumnCards/Card.tsx
+++ ui-complib/lib/components/ColumnCards/Card.tsx
@@ -1,12 +1,32 @@
+// ColumnCard.tsx
+import React from 'react';
 
-function Card(props: { title: string }) {
-    return (
-        <h1 className="p-2 md:p-6 bg-white">{props.title}</h1>
-    );
+interface CardProps {
+  title: string;
+  description: string;
+  imageUrl: string;
+  link: string;
 }
 
-// responsive - mobile first
-// tailwind css, own css
-// npm build, publish
+interface ColumnCardProps {
+  cards: CardProps[];
+}
 
-export { Card };
+function ColumnCard({ cards }: ColumnCardProps): React.ReactNode {
+  return (
+    <div className="flex">
+      {cards.map((card, index) => (
+        <a key={index} href={card.link} className="flex-1 p-4 text-center hover:bg-gray-100 transition duration-300 ease-in-out">
+          <img src={card.imageUrl} alt={card.title} className="mb-2" />
+          <h2 className="text-xl font-semibold mb-2 text-black-500">
+            {card.title}
+          </h2>
+          <p className="text-sm text-gray-500 mb-4">{card.description}</p>
+          <p className="text-blue-500">{card.link}</p>
+        </a>
+      ))}
+    </div>
+  );
+}
+
+export default ColumnCard;
